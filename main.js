b4a8b6934376773c475a520aea4ee40e9dcbb456
ikay001 <ikennaisaac14@gmail.com>
2023-01-30 04:33:25 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// let arrayofuser =["ikay","emma","prine","you","me"];
+
+// //loop through the arrayofusers
+// console.log(arrayofuser)
+
+let arrayofuser =["undefined","null","objects","array","loop",];
+
+//loop through the arrayofusers
+console.log(arrayofuser)
